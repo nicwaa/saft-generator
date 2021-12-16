c51d643d52214e5ac9b4e2b94c5598dd1934e5bb
nbw <Nicholasb.waage@gmail.com>
2021-12-16 15:48:34 +0100

    xml templates finished

--- generator.py
+++ generator.py
@@ -1,38 +1,55 @@
 from xml.etree.ElementTree import Element, SubElement, Comment, tostring, fromstring, dump
-import lxml.etree
 from utils import prettify
+from dataclasses import dataclass, make_dataclass
 
 
-class SaftGenerator:
+class XmlGenerator:
+
+    audit_attr = {
+        'xmlns': "urn:StandardAuditFile-Taxation-CashRegister:NO",
+        'xmlns:xsi': "http://www.w3.org/2001/XMLSchema-instance",
+        'xsi:schemaLocation': "urn:StandardAuditFile-Taxation-CashRegister:NO saft.xsd"
+    }
 
     def __init__(self, root_tag):
-        self.root = Element(root_tag)
-        with open(f'xml-files/{root_tag}.xml', 'r') as f:
-            for element in fromstring(f.read()).iter():
-                if element.tag == root_tag: continue
-                SubElement(self.root, element.tag)
+        if root_tag == 'auditfile':
+            self.root = Element(root_tag, attrib=self.audit_attr)
+        else:
+            self.root = Element(root_tag)
+            with open(f'xml-templates/{root_tag}.xml', 'r') as f:
+                for element in fromstring(f.read()).iter():
+                    if element.tag == root_tag:
+                        continue
+                    SubElement(self.root, element.tag)
 
-    def add_sub_element(self, element_tag, inp=None):
-        SubElement(self.root, element_tag).text = str(inp) if inp is not None else ''
+    def __repr__(self):
+        return prettify(self.root)
+
+    def add_sub_element(self, tag, text=None):
+        SubElement(self.root, tag).text = str(text) if text is not None else ''
 
     def fill_sub_element(self, tag, text):
-        self.root.find(tag).text = str(text) if type(text).__name__ == 'int' else text
+        self.root.find(tag).text = str(text) if type(text).__name__ != 'str' else text
 
-    def remove_sub_element(self, sub_element_tag):
+    def append_sub_elements(self, target_tag, source_tag):
+        self.root.find(target_tag).append(XmlGenerator(source_tag).root)
+
+    def remove_sub_element(self, tag):
         try:
-            self.root.remove(self.root.find('softwareDesc'))
+            self.root.remove(self.root.find(tag))
         except TypeError as e:
-            print('STOOOOOR FEIL!!!', e)
-
-    def print_tree(self):
-        print(prettify(self.root))
+            print('ikke spis rå poteter', e)
 
 
 if __name__ == '__main__':
-    header = SaftGenerator('header')
+    header = XmlGenerator('header')
     # test.add_sub_element('startDate', '2020-01-01')
-    header.remove_sub_element('softwareDesc')
+    # header.remove_sub_element('timeCreated')
+    header.append_sub_elements('fiscalYear', 'header')
 
-    header.print_tree()
+    print(header)
+
+    # for i in header.root.iter():
+    #     print(i.tag)
 
 
--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 
 from xml.etree.ElementTree import Element, SubElement, Comment, tostring, dump, canonicalize, fromstring
 from xml.dom.minidom import parseString
-from generator import SaftGenerator
+from generator import XmlGenerator
 from utils import prettify
 
-auditfile_attributes = {
-    'xmlns': "urn:StandardAuditFile-Taxation-CashRegister:NO",
-    'xmlns:xsi': "http://www.w3.org/2001/XMLSchema-instance",
-    'xsi:schemaLocation': "urn:StandardAuditFile-Taxation-CashRegister:NO saft.xsd"
-}
+# Generate base tag
+auditfile = XmlGenerator('auditfile')
 
-auditfile = Element('auditfile', attrib=auditfile_attributes)
-
-header = SaftGenerator('header')
-
+# Generate header
+header = XmlGenerator('header')
 header.fill_sub_element('fiscalYear', 2020)
-header.fill_sub_element('startDate', '2020-1-1')
-header.fill_sub_element('endDate', '2020-1-31')
+header.fill_sub_element('startDate', '2020-01-01')
+header.fill_sub_element('endDate', '2020-01-31')
 header.fill_sub_element('curCode', 'NOK')
 header.fill_sub_element('dateCreated', '2021-03-30')
 header.fill_sub_element('timeCreated', '10:40:00')
@@ -25,17 +20,20 @@ header.fill_sub_element('softwareVersion', '3.14.1.3')
 header.fill_sub_element('softwareCompanyName', 'Kassasystemer AS')
 header.fill_sub_element('auditfileVersion', '1.0')
 
-auditfileSender = SaftGenerator('auditfileSender')
+auditfileSender = XmlGenerator('auditfileSender')
+auditfileSender.fill_sub_element('taxRegistrationCountry', 'NO')
+auditfileSender.fill_sub_element('companyName', 'big popo')
 
 header.root.append(auditfileSender.root)
+auditfile.root.append(header.root)
 
-auditfile.append(header.root)
+company = XmlGenerator('company')
+company.append_sub_elements('customersSuppliers', 'customerSupplier')
+auditfile.root.append(company.root)
 
-print(prettify(auditfile))
-#
-# dump(auditfile)
-#
-# print(canonicalize(tostring(auditfile)))
-#
-with open('output.xml', 'wb') as f:
-    f.write(prettify(auditfile).encode())
+auditfile.remove_sub_element('header')
+
+print(auditfile)
+
+with open('output.xml', 'w') as f:
+    f.write(str(auditfile))
